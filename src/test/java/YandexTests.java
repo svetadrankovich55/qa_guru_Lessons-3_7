ab71b1c152333289b59f43b082304b702c3ef5cc
Stepan Chepil <stepan.chepil@gmail.com>
2020-12-02 15:23:18 +0200

    Add YandexTests.java

---
+++ src/test/java/YandexTests.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTests {
+
+     @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://yandex.ru");
+
+        // Ввести Selenide в поиск
+        $("#text").setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(Condition.text("selenide.org"));
+    }
+}
